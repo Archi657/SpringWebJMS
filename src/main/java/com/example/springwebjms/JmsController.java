67b2197e6388a2d8a1b08f38534d1034b3585923
Julian <40327956+Archi657@users.noreply.github.com>
2024-01-01 13:37:29 -0500

    response site /send added and new api route

--- src/main/java/com/example/springwebjms/JmsController.java
+++ src/main/java/com/example/springwebjms/JmsController.java
@@ -1,6 +1,8 @@
 package com.example.springwebjms;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -9,8 +11,16 @@ import org.springframework.web.bind.annotation.RestController;
 public class JmsController {
     @Autowired
     private JmsSender sender;
-    @PostMapping("/send")
-    public void sendMessage(@RequestBody String message){
-        sender.sendMessage(message);
+    @PostMapping("/api/send")
+    public ResponseEntity<String> sendMessage(@RequestBody String message){
+        try {
+            sender.sendMessage(message);
+            // Return a success response with status 200
+            return ResponseEntity.status(HttpStatus.OK).body("Message sent successfully");
+        } catch (Exception e) {
+            // Return an error response with status 500
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Error sending message");
+        }
     }
+
 }
--- src/main/java/com/example/springwebjms/MainController.java
+++ src/main/java/com/example/springwebjms/MainController.java
@@ -1,12 +1,28 @@
 package com.example.springwebjms;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class MainController {
+    @Autowired
+    private JmsSender sender;
     @GetMapping("")
     public String showHomePage(){
         return "index";
     }
+
+    @PostMapping("/send")
+    public String sendMessage(@RequestBody String message, String comment, Model model) {
+        sender.sendMessage(comment);
+        model.addAttribute("processedComment", comment);
+
+        return "send";
+    }
+
 }
